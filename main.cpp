07dd50227e9169f8793729b3758141bd49721347
Congliang0229 <15150059801@mail.nwpu.edu.cn>
2022-11-11 17:43:55 +0800

    second commit

--- main.cpp
+++ main.cpp
@@ -40,8 +40,8 @@ int main() {
     cout << "请输入公私钥位数:" << endl;
     string s;
     cin >> s;
-    int i = 0;
-    RSA rsa;
+//    int i = 0;
+//    RSA rsa;
 
     while (!AllisNum(s)) {
         cout << "输入有误!请输入纯数字" << endl;
@@ -49,113 +49,113 @@ int main() {
     }
     int digit = stoi(s);
 
-    Clock::start();
-    while (i++ != 100) {
-        rsa.start(digit);
-    }
-    Clock::end();
-
-
-//
-//    RSA rsa;
-//    cout << "正在生成" << digit << "位公私钥，请稍候......" << endl;
-//    rsa.start(digit);
-//    while (true) {
-//        show_menu();
-//
-//        char c;
-//        cin >> c;
-//        while (!(c >= '0' && c <= '6')) {
-//            cout << "输入有误!请重新输入!" << endl;
-//            cin >> c;
-//        }
-//        int choice = c - '0';
-//        if (choice == 1) {
-//            cout << "======================================================================" << endl;
-//            cout << "开始加密                                                                " << endl;
-//            cout << "请输入需要加密的明文(字母、数字或符号):                                       " << endl;
-//            string M;
-//            cin.ignore();
-//            getline(cin, M);
-//            vector<BigNumber> C = rsa.encrypt_by_string(M, true);
-//            cout << "加密完成,密文如下:                                                         " << endl;
-//            for (int i = 0; i < C.size(); ++i) {
-//                cout << C[i] << endl;
-//            }
-////            cout << "======================================================================" << endl;
-//        } else if (choice == 2) {
-//            cout << "======================================================================" << endl;
-//            cout << "开始解密                                                                " << endl;
-//            cout << "请输入需要解密的密文(十六进制数字,可换行,连续输入两次回车结束输入):                " << endl;
-//            vector<BigNumber> C;
-//            string temp;
-//            cin.ignore();
-//            while (getline(cin, temp)) {
-//                if (temp == "") {
-//                    break;
-//                }
-//                BigNumber b(temp);
-//                C.push_back(b);
-//            }
-//            string M = rsa.decrypt_by_string(C, true);
-//            cout << "解密完成,明文如下:                                                         " << endl;
-//            cout << M << endl;
-//
-//
-//        } else if (choice == 3) {
-//            cout << "======================================================================" << endl;
-//            cout << "开始数字签名                                                             " << endl;
-//            cout << "请输入需要签名的明文(字母、数字或符号):                                       " << endl;
-//            string M;
-//            cin.ignore();
-//            getline(cin, M);
-//            vector<BigNumber> C = rsa.encrypt_by_string(M, false);
-//            cout << "签名完成,签名如下:                                                         " << endl;
-//            for (int i = 0; i < C.size(); ++i) {
-//                cout << C[i] << endl;
-//            }
-//        } else if (choice == 4) {
-//            cout << "======================================================================" << endl;
-//            cout << "开始验证签名                                                             " << endl;
-//            cout << "请输入需要验证的签名(十六进制数字,可换行,连续输入两次回车结束输入):                " << endl;
-//            vector<BigNumber> C;
-//            string temp;
-//            cin.ignore();
-//            while (getline(cin, temp)) {
-//                if (temp == "") {
-//                    break;
-//                }
-//                BigNumber b(temp);
-//                C.push_back(b);
-//            }
-//            string M = rsa.decrypt_by_string(C, false);
-//            cout << "验证完成,签名明文如下:                                                         " << endl;
-//            cout << M << endl;
-//        } else if (choice == 5) {
-//            cout << "======================================================================" << endl;
-//            cout << "公钥如下:                                                             " << endl;
-//            cout << "n = " << rsa.n << endl;
-//            cout << "e = " << rsa.e << endl;
-//            cout << "私钥如下:                                                             " << endl;
-//            cout << "d = " << rsa.d << endl;
-//
-//        } else if (choice == 6) {
-//            cout << "======================================================================" << endl;
-//            cout << "开始重置密钥                                                             " << endl;
-//            cout << "请输入公私钥位数:" << endl;
-//            size_t digit;
-//            cin >> digit;
-//            cout << "正在生成" << digit << "位公私钥，请稍候......" << endl;
-//            rsa.start(digit);
-//        } else if (choice == 0) {
-//            cout << "==========================欢迎您下次使用,再见============================" << endl;
-//            return 0;
-//        }
-//
-//
+//    Clock::start();
+//    while (i++ != 100) {
+//        rsa.start(digit);
 //    }
-//
-//    return 0;
+//    Clock::end();
+
+
+
+    RSA rsa;
+    cout << "正在生成" << digit << "位公私钥，请稍候......" << endl;
+    rsa.start(digit);
+    while (true) {
+        show_menu();
+
+        char c;
+        cin >> c;
+        while (!(c >= '0' && c <= '6')) {
+            cout << "输入有误!请重新输入!" << endl;
+            cin >> c;
+        }
+        int choice = c - '0';
+        if (choice == 1) {
+            cout << "======================================================================" << endl;
+            cout << "开始加密                                                                " << endl;
+            cout << "请输入需要加密的明文(字母、数字或符号):                                       " << endl;
+            string M;
+            cin.ignore();
+            getline(cin, M);
+            vector<BigNumber> C = rsa.encrypt_by_string(M, true);
+            cout << "加密完成,密文如下:                                                         " << endl;
+            for (int i = 0; i < C.size(); ++i) {
+                cout << C[i] << endl;
+            }
+//            cout << "======================================================================" << endl;
+        } else if (choice == 2) {
+            cout << "======================================================================" << endl;
+            cout << "开始解密                                                                " << endl;
+            cout << "请输入需要解密的密文(十六进制数字,可换行,连续输入两次回车结束输入):                " << endl;
+            vector<BigNumber> C;
+            string temp;
+            cin.ignore();
+            while (getline(cin, temp)) {
+                if (temp == "") {
+                    break;
+                }
+                BigNumber b(temp);
+                C.push_back(b);
+            }
+            string M = rsa.decrypt_by_string(C, true);
+            cout << "解密完成,明文如下:                                                         " << endl;
+            cout << M << endl;
+
+
+        } else if (choice == 3) {
+            cout << "======================================================================" << endl;
+            cout << "开始数字签名                                                             " << endl;
+            cout << "请输入需要签名的明文(字母、数字或符号):                                       " << endl;
+            string M;
+            cin.ignore();
+            getline(cin, M);
+            vector<BigNumber> C = rsa.encrypt_by_string(M, false);
+            cout << "签名完成,签名如下:                                                         " << endl;
+            for (int i = 0; i < C.size(); ++i) {
+                cout << C[i] << endl;
+            }
+        } else if (choice == 4) {
+            cout << "======================================================================" << endl;
+            cout << "开始验证签名                                                             " << endl;
+            cout << "请输入需要验证的签名(十六进制数字,可换行,连续输入两次回车结束输入):                " << endl;
+            vector<BigNumber> C;
+            string temp;
+            cin.ignore();
+            while (getline(cin, temp)) {
+                if (temp == "") {
+                    break;
+                }
+                BigNumber b(temp);
+                C.push_back(b);
+            }
+            string M = rsa.decrypt_by_string(C, false);
+            cout << "验证完成,签名明文如下:                                                         " << endl;
+            cout << M << endl;
+        } else if (choice == 5) {
+            cout << "======================================================================" << endl;
+            cout << "公钥如下:                                                             " << endl;
+            cout << "n = " << rsa.n << endl;
+            cout << "e = " << rsa.e << endl;
+            cout << "私钥如下:                                                             " << endl;
+            cout << "d = " << rsa.d << endl;
+
+        } else if (choice == 6) {
+            cout << "======================================================================" << endl;
+            cout << "开始重置密钥                                                             " << endl;
+            cout << "请输入公私钥位数:" << endl;
+            size_t digit;
+            cin >> digit;
+            cout << "正在生成" << digit << "位公私钥，请稍候......" << endl;
+            rsa.start(digit);
+        } else if (choice == 0) {
+            cout << "==========================欢迎您下次使用,再见============================" << endl;
+            return 0;
+        }
+
+
+    }
+
+    return 0;
 }
 
 
